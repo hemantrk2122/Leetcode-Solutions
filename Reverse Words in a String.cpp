3b95e28988f7c1f46da0c3a4e0db3d024c6d9062
hemantrk2122 <41506705+hemantrk2122@users.noreply.github.com>
2020-07-16 14:06:15 +0530

    Update Reverse Words in a String.cpp

--- Reverse Words in a String.cpp
+++ Reverse Words in a String.cpp
@@ -1,3 +1,6 @@
+//In this I am using stringstream which allows me to process the string as a collection of word(also a string)
+
+// Here i'm iterrating over all words of my input string and saving that into the string word (using >> operator which indicates the data flow ).
 class Solution {
 public:
     string reverseWords(string s) {
