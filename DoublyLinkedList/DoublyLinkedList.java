6cff8917fc93a908eef354f5851a54cc67b2cc91
hemantrk2122 <hemantrk2122@gmail.com>
2023-06-25 12:35:32 +0530

    Push and print in DLL

--- DoublyLinkedList/DoublyLinkedList.java
+++ DoublyLinkedList/DoublyLinkedList.java
@@ -2,7 +2,12 @@ package DoublyLinkedList;
 
 public class DoublyLinkedList {
     public static void main(String[] args){
-
+        MyDoublyLinkedList list = new MyDoublyLinkedList(1);
+        list.push(2);
+        list.push(3);
+        list.push(4);
+        list.push(5);
+        list.printDLL();
     }
 }
 
@@ -21,5 +26,22 @@ class MyDoublyLinkedList{
     ListNode head;
     MyDoublyLinkedList(int data){
         head = new ListNode(data);
+    }
+
+    void printDLL(){
+        ListNode curr = head;
+		while(curr!=null){
+			System.out.print(curr.data+"<->");
+			curr = curr.next;
+		}
+		System.out.println("null");
+    }
+
+    // inserting an element at start
+    void push(int data){
+        ListNode node = new ListNode(data);
+        node.next = head;
+        head.prev = node;
+        head = node;
     }
 }
