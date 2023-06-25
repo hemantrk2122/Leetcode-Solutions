e42b4a9e08694fc0a6bf65dcdb7401dd232bd273
hemantrk2122 <hemantrk2122@gmail.com>
2023-06-25 17:57:05 +0530

    Circular LinkedList impl

---
+++ CircularLinkedList/CircularLinkedList.java
@@ -0,0 +1,20 @@
+package CircularLinkedList;
+
+class ListNode{
+    int data;
+    ListNode next;
+    ListNode(int data){
+        this.data = data;
+        next = null;
+    }
+}
+
+class MyCircularLinkedList{
+
+}
+
+public class CircularLinkedList {
+    public static void main(String[] args){
+        
+    }
+}
